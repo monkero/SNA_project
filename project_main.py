77ac5f41f1cb2b4d4e1ee58b339f7a65d4395860
Eemeli Kyröläinen <eemeli.ky@gmail.com>
2025-05-08 21:32:39 +0300

    Kaikki koodi valmiiksi

--- project_main.py
+++ project_main.py
@@ -12,16 +12,18 @@ def main():
     print(f"Parsing {input_file} please wait...")
     result, time_ = timed(lambda: keyword_matching(input_file, keywords))
     print(f"Parsing {input_file} took {time_:.1f}s\n")
-    counts, threads = result
+    keywords, threads = result
     if print_interim:
-        print_results(counts, threads)
-    keywords_to_histograms(counts, year)
-    G, network_metrics = build_thread_network(counts, threads, year)
+        print_results(keywords, threads)
+    keywords_to_histograms(keywords, year)
+    G, network_metrics = build_thread_network(keywords, threads, year)
     if show_graph:
         show_network_graph(G)
     network_metrics = analyze_network(G, network_metrics)
     degrees = plot_degree_distribution(G, year)
     powerlaw_fit(network_metrics, degrees, year)
+    analyze_network_with_threshold(keywords, threads, year)
+    analyze_reciprocity(threads, year)
     print("\nDone.")
 
 
--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ def timed(func):
     return result, time() - start
 
 
-def keywords_parser() -> dict[str, dict[str, set[int] | int]]:
+def keywords_parser():
     keywords = {}
     with open(KEYWORDS_FILE, "r", encoding="utf-8") as file:
         for line in file:
@@ -33,7 +33,7 @@ def keywords_parser() -> dict[str, dict[str, set[int] | int]]:
     return keywords
 
 
-def keyword_matching(input_file, keywords) -> dict[str, dict[str, set[int] | int]] and dict[int, str]:
+def keyword_matching(input_file, keywords):
     file_size = Path(input_file).stat().st_size
     est_lines = int((file_size / REF_SIZE) * REF_LINES)
     thread_id = None
@@ -60,15 +60,25 @@ def keyword_matching(input_file, keywords) -> dict[str, dict[str, set[int] | int
                     word = fields[0].lower()
                     lemma = fields[2].lower()
 
-                    # Single-word match
-                    if word in keywords or lemma in keywords:
-                        keywords[word if word in keywords else lemma]['total_count'] += 1
-                        keywords[word if word in keywords else lemma]['thread_ids'].add(thread_id)
+                    # Keyword matching
+                    if word in keywords:
+                        keywords[word]['total_count'] += 1
+                        keywords[word]['thread_ids'].add(thread_id)
                         if thread_id not in threads:
-                            threads[thread_id] = {'title': thread_title,
-                                                  'hits': 1}
+                            threads[thread_id] = {'title': thread_title}
+                        if word not in threads[thread_id]:
+                            threads[thread_id][word] = 1
                         else:
-                            threads[thread_id]['hits'] += 1
+                            threads[thread_id][word] += 1
+                    if lemma in keywords:
+                        keywords[lemma]['total_count'] += 1
+                        keywords[lemma]['thread_ids'].add(thread_id)
+                        if thread_id not in threads:
+                            threads[thread_id] = {'title': thread_title}
+                        if lemma not in threads[thread_id]:
+                            threads[thread_id][lemma] = 1
+                        else:
+                            threads[thread_id][lemma] += 1
 
     return keywords, threads
 
@@ -109,14 +119,33 @@ def powerlaw_fit(network_metrics, degrees, year):
 
 
 def analyze_network(G, network_metrics):
+    """
+    LCC = Largest connected component
+    metrics = {
+        'nodes',
+        'edges',
+        'maximum degree',
+        "average degree",
+        'global clustering coefficient',
+        'diameter',
+        'LCC diameter',
+        'average path length',
+        'community sizes',
+        'number of communities',
+        'modularity',
+        'LCC size',
+        'average degree centrality'
+        'follows powerlaw'
+    }
+    """
     print("\nNETWORK ANALYSIS:")
-    avg_deg = sum(G.degree(node) for node in G.nodes) / len(G.nodes)
-    network_metrics['average degree'] = avg_deg
-    print(f"Average degree: {avg_deg:.3f}")
-
     max_deg = max(G.degree(), key=lambda x: x[1])[1]
     network_metrics['maximum degree'] = max_deg
     print(f"Maximum degree: {max_deg}")
+
+    avg_deg = sum(G.degree(node) for node in G.nodes) / len(G.nodes)
+    network_metrics['average degree'] = avg_deg
+    print(f"Average degree: {avg_deg:.3f}")
 
     global_cc = nx.transitivity(G)
     network_metrics['global clustering coefficient'] = global_cc
@@ -132,7 +161,20 @@ def analyze_network(G, network_metrics):
     lcc = max(nx.connected_components(G), key=len)
     lcc_graph = G.subgraph(lcc).copy()
     lcc_diameter = nx.diameter(lcc_graph)
+    lcc_size = len(lcc)
     network_metrics['LCC diameter'] = lcc_diameter
+    print(f"Largest connected component diameter: {lcc_diameter}")
+
+    if nx.is_connected(G) and len(G.nodes) > 1:
+        avg_path_length = nx.average_shortest_path_length(G)
+    else:
+        if lcc_size > 1:
+            avg_path_length = nx.average_shortest_path_length(lcc_graph)
+        else:
+            avg_path_length = 0
+
+    network_metrics['average path length'] = avg_path_length
+    print(f"Average path length: {avg_path_length}")
 
     partition = cl.community_louvain.best_partition(G)
     communities = {}
@@ -140,16 +182,25 @@ def analyze_network(G, network_metrics):
         communities.setdefault(id_, []).append(node)
     modularity = cl.modularity(partition, G)
     communities = list(communities.values())
-    communities_sizes = [len(c) for c in communities]
-    giant_comm = max(communities_sizes)
-    network_metrics['giant community size'] = giant_comm
-    print(f"Giant community size: {giant_comm}")
+    communities_sizes = sorted([len(c) for c in communities], reverse=True)
+
+    network_metrics['community sizes'] = communities_sizes
+    print(f"Community sizes: {communities_sizes}")
 
     network_metrics['number communities'] = len(communities_sizes)
     print(f"Number communities: {len(communities_sizes)}")
 
     network_metrics['modularity'] = modularity
     print(f"Modularity (quality): {modularity:.3f}")
+
+    network_metrics['LCC size'] = lcc_size
+    print(f"Largest connected component size: {lcc_size}")
+
+    deg_cent = nx.degree_centrality(G)
+    avg_deg_cent = sum(deg_cent.values()) / len(deg_cent)
+    network_metrics['average degree centrality'] = avg_deg_cent
+    print(f"Average degree centrality: {avg_deg_cent:.3f}")
+
     return network_metrics
 
 
@@ -274,3 +325,156 @@ def keywords_to_histograms(results, year):
         plt.savefig(fig_path)
     plt.show()
     plt.close()
+
+
+def analyze_network_with_threshold(keywords, threads, year):
+    k_values = [2, 3, 5, 10]
+    metrics = {
+        'k': k_values,
+        'nodes': [],
+        'edges': [],
+        'maximum degree': [],
+        "average degree": [],
+        'global clustering coefficient': [],
+        'LCC diameter': [],
+        'average path length': [],
+        'community sizes': [],
+        'number communities': [],
+        'modularity': [],
+        'LCC size': [],
+        'average degree centrality': []
+    }
+
+    for k in k_values:
+        G = nx.Graph()
+        G.add_nodes_from(threads.keys())
+        for keyword, data in keywords.items():
+            thread_ids = data['thread_ids']
+            for t1, t2 in combinations(thread_ids, 2):
+                if G.has_edge(t1, t2):
+                    continue
+                if threads[t1][keyword] > k - 1 and threads[t2][keyword] > k - 1:
+                    G.add_edge(t1, t2)
+        metrics['nodes'].append(G.number_of_nodes())
+        metrics['edges'].append(G.number_of_edges())
+        calculate_metrics(metrics, G)
+    display_metrics(metrics, year)
+
+
+def display_metrics(metrics, year):
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = str(os.path.join(current_path, "results\\" + year))
+    plt.figure(figsize=(12, 8))
+    for key in ['nodes', 'edges', 'maximum degree',
+                'LCC size', 'number communities']:
+        plt.plot(metrics['k'], metrics[key], marker='o', label=key)
+
+    plt.xlabel('Threshold k')
+    plt.ylabel('Value')
+    plt.title('Core Network Metrics vs Threshold k (figure 1)')
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.grid(True)
+    plt.tight_layout()
+    fig_path = os.path.join(file_path, f"{year}_threshold_k_figure_(1).png")
+    if not os.path.exists(fig_path):
+        plt.savefig(fig_path)
+    plt.show()
+    plt.close()
+
+    plt.figure(figsize=(12, 8))
+    for key in ['average degree centrality', 'modularity', 'average path length', 'LCC diameter',
+                'average degree', 'global clustering coefficient']:
+        plt.plot(metrics['k'], metrics[key], marker='s', label=key)
+
+    plt.xlabel('Threshold k')
+    plt.ylabel('Value')
+    plt.title('Core Network Metrics vs Threshold k (figure 2)')
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.grid(True)
+    plt.tight_layout()
+    fig_path = os.path.join(file_path, f"{year}_threshold_k_figure_(2).png")
+    if not os.path.exists(fig_path):
+        plt.savefig(fig_path)
+    plt.show()
+    plt.close()
+
+
+def calculate_metrics(metrics, G):
+    max_deg = max(G.degree(), key=lambda x: x[1])[1]
+    avg_deg = sum(G.degree(node) for node in G.nodes) / len(G.nodes)
+    gcc = nx.transitivity(G)
+
+    components = list(nx.connected_components(G))
+    giant_comp = max(components, key=len)
+    giant_size = len(giant_comp)
+
+    if nx.is_connected(G) and len(G.nodes) > 1:
+        avg_path_length = nx.average_shortest_path_length(G)
+        diameter = nx.diameter(G)
+    else:
+        # Compute for the giant component if exists
+        if giant_size > 1:
+            sub = G.subgraph(giant_comp)
+            avg_path_length = nx.average_shortest_path_length(sub)
+            diameter = nx.diameter(sub)
+        else:
+            avg_path_length = 0
+            diameter = 0
+
+    deg_cent = nx.degree_centrality(G)
+    avg_deg_cent = sum(deg_cent.values()) / len(deg_cent)
+
+    partition = cl.community_louvain.best_partition(G)
+    communities = {}
+    for node, id_ in partition.items():
+        communities.setdefault(id_, []).append(node)
+    try:
+        modularity = cl.modularity(partition, G)
+    except ValueError:
+        modularity = 0
+    communities = list(communities.values())
+    communities_sizes = [len(c) for c in communities]
+
+    metrics['maximum degree'].append(max_deg)
+    metrics['average degree'].append(avg_deg)
+    metrics['global clustering coefficient'].append(gcc)
+    metrics['LCC diameter'].append(diameter)
+    metrics['average path length'].append(avg_path_length)
+    metrics['community sizes'].append(communities_sizes)
+    metrics['number communities'].append(len(communities_sizes))
+    metrics['modularity'].append(modularity)
+    metrics['LCC size'].append(giant_size)
+    metrics['average degree centrality'].append(avg_deg_cent)
+
+
+def analyze_reciprocity(threads, year):
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = str(os.path.join(current_path, "results\\" + year))
+    even = 0
+    all_threads = len(threads.keys())
+    for tid in threads.keys():
+        count = 0
+        for kw in threads[tid].keys():
+            if not kw == 'title':
+                count += threads[tid][kw]
+        if (count & 1) == 0:
+            even += 1
+    odd = all_threads - even
+
+    categories = ['Even Amount of Threads', 'Odd Amount of Threads']
+    counts = [even, odd]
+    colors = ['#66b3ff', '#ff9999']
+
+    plt.bar(categories, counts, color=colors)
+    plt.xlabel('Thread Type')
+    plt.ylabel('Number of Threads')
+    plt.title('Distribution of Even/Odd Amount of Keywords in Threads')
+
+    for i, count in enumerate(counts):
+        plt.text(i, count + 0.1, str(count), ha='center')
+
+    fig_path = os.path.join(file_path, f"{year}_even_odd_figure.png")
+    if not os.path.exists(fig_path):
+        plt.savefig(fig_path)
+    plt.show()
+
